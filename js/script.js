199bd7901ab9deb250f61750b3cb7bc9d09e43a5
kagero <lj.chistova@yandex.ru>
2017-09-09 19:23:54 +0300

    7.8

---
+++ js/script.js
@@ -0,0 +1,11 @@
+	var search = document.querySelector(".btn-searching");
+	var slide = document.querySelector(".form-show");
+	
+	document.addEventListener("DOMContentLoaded", function (loading){
+		slide.classList.toggle("form-hidden");		
+	});
+	
+	search.addEventListener("click", function (evt){
+		evt.preventDefault();
+		slide.classList.toggle("form-hidden");
+	});
